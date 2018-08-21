e2452fd4b0be381dced59aa5296980b3d2b370bf
data-tracker <data-tracker@outlook.com>
2018-08-22 00:00:57 +0800

    add some string combine and csv read code

--- airpolt-prog.R
+++ airpolt-prog.R
@@ -1,10 +1,25 @@
 # air pollution pollutant mean
 # test code
+pollut.data = read.csv("specdata/001.csv", sep = ",", skipNul = TRUE)
 pollut.data = read.csv("specdata/001.csv", sep = ",")
 nrow(pollut.data)
 ncol(pollut.data)
 pollut.data [2, ]
 
-pollut.mean <- function(dir, pollutant, id = 1:332) {
+pollut.mean <- function(dir, pollutant, id = 1:3) {
   
+    idx = sprintf("%03d", id)
+    fname = paste(dir, "/", idx, ".csv", sep = "")
+    fname
+    length(fname)
+    read.csv(fname[1], sep = ",")
+#    for (i in length(fname)) {
+#        idx.dframe = read.csv(fname[i], sep = ",")
+#        idx.dframe = idx.dframe[, is.na(pollutant) == FALSE]
+#        idx.dframe
+#    }
+#    idx.dframe = read.csv(fname, sep = ",")
+#    idx.dframe
+#    attach(idx.dframe)
+#    valid_pollut = idx.dframe
 }
