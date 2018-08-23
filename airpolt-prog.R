cecd5fb7634ddf594635cc1ffbfffa02878c9ccf
data-tracker <data-tracker@outlook.com>
2018-08-23 18:49:18 +0800

    calculate pollut mean of each csv file

--- airpolt-prog.R
+++ airpolt-prog.R
@@ -7,19 +7,19 @@ ncol(pollut.data)
 pollut.data [2, ]
 
 pollut.mean <- function(dir, pollutant, id = 1:3) {
-  
+    
+    idx.mean = c(id)
     idx = sprintf("%03d", id)
     fname = paste(dir, "/", idx, ".csv", sep = "")
-    fname
-    length(fname)
-    read.csv(fname[1], sep = ",")
-#    for (i in length(fname)) {
-#        idx.dframe = read.csv(fname[i], sep = ",")
-#        idx.dframe = idx.dframe[, is.na(pollutant) == FALSE]
-#        idx.dframe
-#    }
-#    idx.dframe = read.csv(fname, sep = ",")
-#    idx.dframe
-#    attach(idx.dframe)
-#    valid_pollut = idx.dframe
+    for (i in id) {
+      idx.df = read.csv(fname[i], sep = ",")
+      idx.df = idx.df[complete.cases(idx.df[,pollutant]),]
+      pollut = idx.df[[pollutant]]
+      idx.mean[i] = sum(pollut)/length(pollut)
+      print(paste("mean val:", i, idx.mean[i]))
+    }
+    print(idx.mean)
 }
+
+pollut.mean(dir = "specdata", pollutant = "sulfate", id = 1:10)
+
